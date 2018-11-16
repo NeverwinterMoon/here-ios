8bd94274ef18ae16c3504137ffe4df24d87e05fd
joehattori <joe4.ninjahattori@icloud.com>
2018-11-16 23:43:24 +0900

    Initial Commit

---
+++ NowHere/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NowHere
+//
+//  Created by 服部穣 on 2018/11/16.
+//  Copyright © 2018 服部穣. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

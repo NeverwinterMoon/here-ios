bfdd99e7703f20d03d7b90d19d667f9bd02e4164
joehattori <joe4.ninjahattori@icloud.com>
2018-11-19 16:49:01 +0900

    show the map (basic parts)

--- Source/AppScreen/Map/MapViewController.swift
+++ Source/AppScreen/Map/MapViewController.swift
@@ -8,30 +8,74 @@
 
 import UIKit
 import AppFoundation
+import CoreLocation
 import FlexLayout
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            
+            self.locationManager.delegate = self
+            self.locationManager.requestAlwaysAuthorization()
+            self.locationManager.requestWhenInUseAuthorization()
+            self.locationManager.startUpdatingLocation()
+            
+            self.checkLocationAuthorization()
+        } else {
+            
+            // show alert
+        }
+        
         self.flexLayout()
     }
     
-    @available(*, unavailable)
-    required init?(coder aDecoder: NSCoder) { fatalError() }
+    override func viewDidLayoutSubviews() {
+        
+        super.viewDidLayoutSubviews()
+        view.flex.layout()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let currentLocation = locations.last!
+        let center = CLLocationCoordinate2D(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 5000, longitudinalMeters: 5000)
+        self.mapView.setRegion(region, animated: true)
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse:
+            self.mapView.showsUserLocation = true
+        case .denied, .notDetermined, .restricted:
+//            show some alert, but get the locations of friends
+            break
+        }
+    }
 
     // MARK: - Private
     private let mapView = MKMapView()
+    private let locationManager = CLLocationManager()
     
     private func flexLayout() {
-        
-        view.flex.define { flex in
-            flex.addItem(self.mapView).padding(0)
+
+        self.view.flex.define { flex in
+            
+            flex.addItem().grow(1).define { flex in
+                flex.addItem(self.mapView).grow(1)
+            }
         }
     }
 }

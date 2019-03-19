d994fb699ccd75c89c0e9b414fee71f1c77825d6
joehattori <joe4.ninjahattori@icloud.com>
2019-03-19 23:29:11 +0900

    create AppLabel

---
+++ Source/AppUIKit/AppLabel.swift
@@ -0,0 +1,21 @@
+//
+//  AppLabel.swift
+//  AppUIKit
+//
+//  Created by 服部穣 on 2019/03/19.
+//  Copyright © 2019 服部穣. All rights reserved.
+//
+
+import UIKit
+
+class AppLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
